c3998d82065ae750786721e227ab2e54fa4f2e2d
viniciosragazzidev <devviniciosragazzi@gmail.com>
2024-06-28 20:09:17 -0300

    Fix Data

--- src/app/(system)/components/select-unit/action.ts
+++ src/app/(system)/components/select-unit/action.ts
@@ -2,29 +2,34 @@ import { ProfileType } from "@/lib/@types";
 import { auth } from "../../../../../auth";
 
 export const getCompanyAndUnits = async () => {
-  const session = await auth();
-  const user = session?.user;
-  const profileId = user?.profileId;
-  const profileFetch = await fetch(
-    `${
-      process.env.PATHNAME || "http://localhost:3000/"
-    }/api/profile/${profileId}`,
-    {
-      method: "GET",
-      next: {
-        revalidate: 1000,
-        tags: ["profile"],
-      },
-    }
-  ).then((res) => res.json());
+  try {
+    const session = await auth();
+    const user = session?.user;
+    const profileId = user?.profileId;
+    const profileFetch = await fetch(
+      `${
+        process.env.PATHNAME || "http://localhost:3000/"
+      }/api/profile/${profileId}`,
+      {
+        method: "GET",
+        next: {
+          revalidate: 1000,
+          tags: ["profile"],
+        },
+      }
+    ).then((res) => res.json());
 
-  const profile: ProfileType = profileFetch.profile;
+    const profile: ProfileType = profileFetch.profile;
 
-  const units = profile?.Unit;
-  const company = profile?.Company;
+    const units = profile?.Unit;
+    const company = profile?.Company;
 
-  return {
-    units,
-    company,
-  };
+    return {
+      units,
+      company,
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
