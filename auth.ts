5a157a5e3351fa1bd42868090d87df176658ed62
viniciosragazzidev <devviniciosragazzi@gmail.com>
2024-06-28 19:23:34 -0300

    Auth e criação de perfil, compania e primeira unidade adicionada

--- auth.ts
+++ auth.ts
@@ -2,15 +2,10 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import db from "@/lib/services/db";
 
-const prisma = new PrismaClient();
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.AUTH_SECRET,
-  // session: {
-  //   strategy: "jwt",
-  // },
   providers: [Google],
 });
--- next-auth.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "next-auth" {
-  interface Session {
-    user: User & {
-      profileId: string;
-    };
-  }
-}
